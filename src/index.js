70568527b0444b2c7872686f263a18be585eea22
Lena Pavlenko <elpav1995@gmail.com>
2021-12-28 17:59:18 +0300

    feat(slider.js, index.js): new feat

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import buttonScroll from "./modules/buttonScroll";
 import calculator from "./modules/calculator";
 import formValidate from "./modules/formValidate";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer('25 december 2021');
 menu();
@@ -13,3 +14,4 @@ buttonScroll();
 calculator();
 formValidate();
 tabs();
+slider();
